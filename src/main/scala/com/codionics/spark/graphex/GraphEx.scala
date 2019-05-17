20a1a86af9b65ec424071c5c4d04179b8eab6686
Manoj Waikar <manoj.waikar@intercax.com>
2019-05-17 16:53:00 +0530

    Partially working hello world app.

--- src/main/scala/com/codionics/spark/graphex/GraphEx.scala
+++ src/main/scala/com/codionics/spark/graphex/GraphEx.scala
@@ -1,39 +1,36 @@
 package com.codionics.spark.graphex
 
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.graphx._
-import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession}
+import org.apache.spark.sql.SparkSession
 
 object GraphEx {
 
+  val nodesFile = "src/main/resources/transport-nodes.csv"
+  val edgesFile = "src/main/resources/transport-relationships.csv"
+
   def main(args: Array[String]) {
-    println(System.getProperty("user.dir"))
-    val master = "local[*]"
-    val appName = "Simple GraphX Application"
 
-    val nodesFile = "./data/transport-nodes.csv"
-    val edgesFile = "./data/transport-relationships.csv"
+    val spark = SparkSession.builder
+      .appName("Simple GraphX Application")
+      .config("spark.master", "local")
+      .getOrCreate()
 
-    val conf = new SparkConf().setAppName(appName).setMaster(master)
-    val sc = new SparkContext(conf)
+    val nodesData = spark.read
+      .format("csv")
+      .option("header", "true")
+      .load(nodesFile)
+    nodesData.show()
 
-    val sparkSession = SparkSession.builder().appName(appName).master(master).getOrCreate()
-
-    val nodesData: DataFrame = readFile(sparkSession, nodesFile)
-    val edgesData: DataFrame = readFile(sparkSession, edgesFile)
+    val edgesData = spark.read
+      .format("csv")
+      .option("header", "true")
+      .load(edgesFile)
+    edgesData.show()
 
     // val logFile = "YOUR_SPARK_HOME/README.md" // Should be some file on your system
     // val logData = spark.read.textFile(logFile).cache()
     // val numAs = logData.filter(line => line.contains("a")).count()
     // val numBs = logData.filter(line => line.contains("b")).count()
     // println(s"Lines with a: $numAs, Lines with b: $numBs")
-//    spark.stop()
-
-    sparkSession.stop()
-    sc.stop()
-  }
-
-  def readFile(sparkSession: SparkSession, path: String): DataFrame = {
-    sparkSession.read.format("csv").option("header", "true").load(path).cache()
+    spark.stop()
   }
 }
