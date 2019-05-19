513c89d1e16aac4a8bdcdfcdf48fbf60743be97f
Manoj Waikar <manoj.waikar@intercax.com>
2019-05-19 19:02:34 +0530

    Partially working GraphFrame.

--- src/main/scala/com/codionics/spark/graphex/GraphEx.scala
+++ src/main/scala/com/codionics/spark/graphex/GraphEx.scala
@@ -1,6 +1,7 @@
 package com.codionics.spark.graphex
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.graphframes._
 
 object GraphEx {
 
@@ -14,23 +15,44 @@ object GraphEx {
       .config("spark.master", "local")
       .getOrCreate()
 
-    val nodesData = spark.read
-      .format("csv")
-      .option("header", "true")
-      .load(nodesFile)
-    nodesData.show()
+    try {
 
-    val edgesData = spark.read
-      .format("csv")
-      .option("header", "true")
-      .load(edgesFile)
-    edgesData.show()
+      val nodes: DataFrame = spark.read
+        .format("csv")
+        .option("header", "true")
+        .load(nodesFile)
+      nodes.show()
 
-    // val logFile = "YOUR_SPARK_HOME/README.md" // Should be some file on your system
-    // val logData = spark.read.textFile(logFile).cache()
-    // val numAs = logData.filter(line => line.contains("a")).count()
-    // val numBs = logData.filter(line => line.contains("b")).count()
-    // println(s"Lines with a: $numAs, Lines with b: $numBs")
-    spark.stop()
+      val edges: DataFrame = spark.read
+        .format("csv")
+        .option("header", "true")
+        .load(edgesFile)
+      // val edges = spark.read.csv(edgesFile, "header = true")
+      edges.show()
+
+      val reversed_edges = edges
+        .withColumn("newSrc", edges.col("dst"))
+        .withColumn("newDst", edges.col("src"))
+        .drop("dst", "src")
+        .withColumnRenamed("newSrc", "src")
+        .withColumnRenamed("newDst", "dst")
+        .select("src", "dst", "relationship", "cost")
+
+      val relationships = edges.union(reversed_edges)
+      relationships.show()
+
+      val g = GraphFrame(nodes, relationships)
+
+      // using BFS
+      println("using BFS")
+      g.vertices
+        .filter("population > 100000 and population < 3000000")
+        .sort("population")
+        .show()
+
+    } finally {
+      spark.stop()
+    }
+
   }
 }
